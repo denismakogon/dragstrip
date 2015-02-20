ef7047290a38ea2f6496f3b3aea4023532f96f86
Ilya Pekelny <pekelny@gmail.com>
2015-02-20 19:24:17 +0200

    Static charts

--- statistician.py
+++ statistician.py
@@ -11,8 +11,11 @@ import BaseHTTPServer
 import eventlet
 import socket
 import re
+import collections
 
 import psutil
+from plotly import graph_objs
+from plotly import plotly
 
 from options import conf
 
@@ -62,6 +65,7 @@ def get_foreign_system_info():
         try:
             resp, content = requester.request("http://%s:8080" % i, "GET")
         except socket.error as e:
+            print e
             time.sleep(3)
             return get_foreign_system_info()
         if int(resp['status']) != 200:
@@ -126,16 +130,16 @@ def get_rabbit_info():
 def as_json(file):
     """Open log file and convert a given content to a valid JSON object."""
 
-    with open(file, 'r') as file:
-        try:
+    try:
+        with open(file, 'r+') as file:
             json_obj = "{%s}" % ', '.join(file.readlines())
             json_obj = json_obj.replace('\n', '')
             json_obj = json_obj.replace("'", '"')
             json_obj = re.sub('\d+(L)', '', json_obj)
             return json_obj
-        except IOError:
-            time.sleep(3)
-            return as_json(file)
+    except IOError:
+        time.sleep(3)
+        return as_json(file)
 
 
 def write_to_file(timekey):
@@ -159,29 +163,86 @@ def run_logger():
             break
 
 
+# =============================================================================
+# Plotly handler
+# =============================================================================
+def get_timed(data, key):
+    """Group logging data items by logging time."""
 
-def run_handler():
+    items = [
+        (k, v[key]) for k, v in data.items() if isinstance(v, types.DictType)]
+    t_format = '%Y-%m-%d %H:%M:%S'
+    items = sorted(items, cmp=lambda x, y: cmp(
+        time.strptime(x[0], t_format),
+        time.strptime(y[0], t_format)))
+    try:
+        timeline, values = zip(*[(i[0], i[1].items()) for i in items])
+        values = [zip(*i) for i in zip(*values)]
+        return timeline, [(i[0][0], i[1]) for i in values]
+    except AttributeError:
+        timeline, values = zip(*items)
+        return (timeline,
+                [('Core%d' % (i+1), v) for i, v in enumerate(zip(*values))])
+
+
+def get_rabbit_plotly():
+    """Get rabbit numeric data prepared for charts building with Plotly."""
+
+    data = json.loads(get_rabbit_info())
+    return {i: get_timed(data, i) for i in ('object_totals', 'queue_totals')}
+
+
+def get_system_plotly():
+    """Get system numeric data prepared for charts building with Plotly."""
+
+    data = json.loads(get_system_info())
+    return {k: {i: get_timed(v, i) for i in ('cpu','disk', 'ram')}\
+                     for k, v in data.items()}
+
+
+def build_chart(log, layout_title=''):
+    """Buld plotly chart with given data."""
+
+    time_series = log[0]
+    data = graph_objs.Data([graph_objs.Scatter(x=time_series, y=v, name=name)\
+                            for name, v in log[1]])
+    layout = graph_objs.Layout(title=layout_title)
+    figure = graph_objs.Figure(data=data, layout=layout)
+    print plotly.plot(figure, filename='date-axes', fileopt='new')
+
+
+def build_static_charts():
+    """Iterate though a log data and build static charts with a given values"""
+
+    plotly.sign_in(conf.plotly_name, conf.plotly_api_key)
+
+    for host, logs in get_system_plotly().items():
+        for name, log in logs.items():
+            build_chart(log, '%s: %s' % (host, name))
+
+    for name, log in get_rabbit_plotly().items():
+        build_chart(log, name)
+
+
+def dynamic_charts():
+    # TODO: implement dynamic charts building.
     while True:
-        try:
-            time.sleep(3)
-            system = get_system_info()
-            rabbit = get_rabbit_info()
-# TODO: implement JSON parser to get plotly valid data.
-# TODO: implement plotly worker.
-        except SystemExit, KeyboardInterrupt:
-            break
-        except IOError:
-            print "Log files haven't been generated. Retrying."
-            continue
+        break
 
 
-# TODO: run from client and server.
-def spawn():
+# =============================================================================
+# Run threads
+# =============================================================================
+def spawn(dynamic=False):
     pool = eventlet.greenpool.GreenPool(3)
-    pool.spawn(run_handler)
-    pool.spawn(run_http)
     pool.spawn(run_logger)
-    pool.waitall()
+    pool.spawn(run_http)
+    if dynamic:
+        pool.spawn(dynamic_charts, True)
+    try:
+        pool.waitall()
+    except KeyboardInterrupt, SystemExit:
+        build_static_charts()
 
 
 if __name__ == '__main__':
