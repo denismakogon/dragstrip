e5687c44796fd2d57144910a139808407e41292f
Pekelny Ilya <ipekelny@mirantis.com>
2015-03-06 13:21:45 +0200

    setup requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(name='dragstrip',
 
       packages=['dragstrip', 'dragstrip.cli_serv', 'dragstrip.statistician'],
       package_dir={'dragstrip': 'dragstrip'},
-      scripts=['dragstrip/drag_client', 'dragstrip/drag_server', 'dragstrip/drag_stat'],
+      scripts=['dragstrip/drag_client', 'dragstrip/drag_server',
+               'dragstrip/drag_stat'],
       install_requires=['oslo.messaging>=1.4.0', 'eventlet>=0.16.1',
-                'psutil>=2.2.1', 'plotly>=1.6.6', 'netifaces>=0.10.4', 'httplib2>=0.9'],
-      package_data={'dragstrip': ['rpc.conf', ]},)
+                        'oslo.config>=1.9.0', 'psutil>=2.2.1',
+                        'plotly>=1.6.6', 'netifaces>=0.10.4', 'httplib2>=0.9'],
+      package_data={'dragstrip': ['rpc.conf', ]})
